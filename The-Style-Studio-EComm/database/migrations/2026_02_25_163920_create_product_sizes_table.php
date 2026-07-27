c41e860b0d3dcb8344e8c490763e01faaffcb1e6
Takshil-Thakor7 <takshilthakor7@gmail.com>
2026-07-27 10:04:08 +0530

    Add files via upload

---
+++ The-Style-Studio-EComm/database/migrations/2026_02_25_163920_create_product_sizes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_sizes', function (Blueprint $table) {
+            $table->id('prosize_id');
+            $table->bigInteger('proid');
+             $table->string('size');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+            $table->unique(['proid', 'size']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_sizes');
+    }
+};
